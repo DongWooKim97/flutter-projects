4a455a55b2284cb6eb7288bb420bec8322a96b7b
DongWooKim97 <nanaukim@naver.com>
2023-09-01 10:18:43 +0900

    refactor: WebView 버전업에 따른 파일 수정

--- web_view_android/lib/main.dart
+++ web_view_android/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:web_view_android/screen/home_screen.dart';
 
 void main() {
+  // Flutter 프레임워크가 앱을 실행할 준비가 될 때 까지 기다린다.
+  // main함수에 runApp 함수가 하나밖에 없으면 자동으로 실행되는 것이기에 굳이 사용안해도 된다.
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MaterialApp(
     home: HomeScreen(),
   ));
--- web_view_android/lib/screen/home_screen.dart
+++ web_view_android/lib/screen/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
 
-  WebViewController? controller;
-  final homeUrl = 'https://blog.codefactory.ai';
+  WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
@@ -17,25 +20,25 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         actions: [
           IconButton(onPressed: (){
-            if(controller == null) {
-              return;
-            }
-            controller!.loadUrl(homeUrl);
+            controller.loadRequest(homeUrl);
           }, icon: Icon(
             Icons.home
           ))
         ],
       ),
-      body: WebView(
-        // onWebViewCreated -> 웹뷰가 생성되었을 때 하는 행동. 우리는 class내에 WebviewController를 따로 빼서 관리.
-        onWebViewCreated: (WebViewController controller){
-          this.controller = controller;
-        },
-        // initialUrl : 웹뷰가 실행되면 초기에 어떤 화면을 띄울거냐 하는 것
-        initialUrl: homeUrl,
-        // javascriptMode : 웹뷰에선 자바스크립트모드가 디폴트로 꺼져있음.
-        javascriptMode: JavascriptMode.unrestricted,
+      body: WebViewWidget(
+        controller: controller,
       ),
+      // WebView(
+      //   // onWebViewCreated -> 웹뷰가 생성되었을 때 하는 행동. 우리는 class내에 WebviewController를 따로 빼서 관리.
+      //   onWebViewCreated: (WebViewController controller){
+      //     this.controller = controller;
+      //   },
+      //   // initialUrl : 웹뷰가 실행되면 초기에 어떤 화면을 띄울거냐 하는 것
+      //   initialUrl: homeUrl,
+      //   // javascriptMode : 웹뷰에선 자바스크립트모드가 디폴트로 꺼져있음.
+      //   javascriptMode: JavascriptMode.unrestricted,
+      // ),
     );
   }
 }
