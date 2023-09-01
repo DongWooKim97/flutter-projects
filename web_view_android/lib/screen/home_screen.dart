fe1d43522f2c9f96497e67a041b0629e0ae08cc4
DongWooKim97 <nanaukim@naver.com>
2023-09-01 10:06:10 +0900

    feat: appBar 우측에 홈버튼 및 기능 추가

--- web_view_android/lib/screen/home_screen.dart
+++ web_view_android/lib/screen/home_screen.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+
+  WebViewController? controller;
+  final homeUrl = 'https://blog.codefactory.ai';
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +15,24 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.orange,
         title: Text('Code Factory'),
         centerTitle: true,
+        actions: [
+          IconButton(onPressed: (){
+            if(controller == null) {
+              return;
+            }
+            controller!.loadUrl(homeUrl);
+          }, icon: Icon(
+            Icons.home
+          ))
+        ],
       ),
       body: WebView(
+        // onWebViewCreated -> 웹뷰가 생성되었을 때 하는 행동. 우리는 class내에 WebviewController를 따로 빼서 관리.
+        onWebViewCreated: (WebViewController controller){
+          this.controller = controller;
+        },
         // initialUrl : 웹뷰가 실행되면 초기에 어떤 화면을 띄울거냐 하는 것
-        initialUrl: 'https://blog.codefactory.ai',
+        initialUrl: homeUrl,
         // javascriptMode : 웹뷰에선 자바스크립트모드가 디폴트로 꺼져있음.
         javascriptMode: JavascriptMode.unrestricted,
       ),
