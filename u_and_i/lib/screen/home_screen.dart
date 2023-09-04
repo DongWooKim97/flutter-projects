57a640f3f5f888e3f0774d49c497bd4211458c75
DongWooKim97 <nanaukim@naver.com>
2023-09-04 15:28:56 +0900

    refactor: theme 및 themeData를 통해 유용한 관리 학습

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -9,6 +9,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate =
+      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,28 +22,64 @@ class _HomeScreenState extends State<HomeScreen> {
             width: MediaQuery.of(context).size.width,
             child: Column(
               children: [
-                _TopPart(),
+                _TopPart(
+                  selectedDate: selectedDate,
+                  onPressed: onHeartPressed,
+                ),
                 _BottomPart(),
               ],
             )),
       ),
     );
   }
+
+  void onHeartPressed() {
+    final DateTime now = DateTime.now();
+    // dialog -> date picker (기능 다 만들어져있음)
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true, // 위젯의 사이즈 넘어간 것을 누르면 꺼짐
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            // 플러터의 특징 : 특정 위젯이 어디에 정렬해야하는지 알 수없으면 최대한 사이즈로 먹어버린다. 그래서 현재는 전체화면을 다 먹어버림
+            // align이라는 위젯을 사용해서 어디에 정렬할건지(여기서는 showDialog를) 알려줘야만 한다
+            color: Colors.white,
+            height: 300.0,
+            child: CupertinoDatePicker(
+              // 데이트픽커는 디폴트로 실행하면 now로 실행되는데, 우리는 maximunDate를 now로 했기에에러
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: selectedDate,
+              maximumDate: DateTime(
+                now.year,
+                now.month,
+                now.day,
+              ),
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  selectedDate = date;
+                });
+              }, // 필수로 넣어야함
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
 
-class _TopPart extends StatefulWidget {
-  const _TopPart({super.key});
+class _TopPart extends StatelessWidget {
+  final DateTime selectedDate;
+  final VoidCallback onPressed;
 
-  @override
-  State<_TopPart> createState() => _TopPartState();
-}
-
-class _TopPartState extends State<_TopPart> {
-  DateTime selectedDate =
-      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+  _TopPart({required this.selectedDate, required this.onPressed, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
     final now = DateTime.now();
 
     return Expanded(
@@ -49,64 +88,23 @@ class _TopPartState extends State<_TopPart> {
         children: [
           Text(
             'U&I',
-            style: TextStyle(
-                color: Colors.white, fontFamily: 'parisienne', fontSize: 80.0),
+            style: theme.textTheme.headline1,
           ),
           Column(
             children: [
               Text(
                 '우리 처음 만난날',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 30.0,
-                ),
+                style: textTheme.bodyText1,
               ),
               Text(
                 '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 20.0,
-                ),
+                style: textTheme.bodyText2,
               ),
             ],
           ),
           IconButton(
             iconSize: 60.0,
-            onPressed: () {
-              // dialog -> date picker (기능 다 만들어져있음)
-              showCupertinoDialog(
-                context: context,
-                barrierDismissible: true, // 위젯의 사이즈 넘어간 것을 누르면 꺼짐
-                builder: (BuildContext context) {
-                  return Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      // 플러터의 특징 : 특정 위젯이 어디에 정렬해야하는지 알 수없으면 최대한 사이즈로 먹어버린다. 그래서 현재는 전체화면을 다 먹어버림
-                      // align이라는 위젯을 사용해서 어디에 정렬할건지(여기서는 showDialog를) 알려줘야만 한다
-                      color: Colors.white,
-                      height: 300.0,
-                      child: CupertinoDatePicker(
-                        // 데이트픽커는 디폴트로 실행하면 now로 실행되는데, 우리는 maximunDate를 now로 했기에에러
-                        mode: CupertinoDatePickerMode.date,
-                        initialDateTime: selectedDate,
-                        maximumDate: DateTime(
-                          now.year,
-                          now.month,
-                          now.day,
-                        ),
-                        onDateTimeChanged: (DateTime date) {
-                          setState(() {
-                            selectedDate = date;
-                          });
-                        }, // 필수로 넣어야함
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
+            onPressed: onPressed,
             icon: Icon(
               Icons.favorite,
               color: Colors.red[700],
@@ -118,12 +116,7 @@ class _TopPartState extends State<_TopPart> {
                   now.month,
                   now.day,
                 ).difference(selectedDate).inDays + 1}',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'sunflower',
-              fontWeight: FontWeight.w700,
-              fontSize: 50.0,
-            ),
+            style: textTheme.headline2,
           ),
         ],
       ),
