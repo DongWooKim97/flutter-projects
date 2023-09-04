ce5a1c84ebaf782bd8e57652ab36207d7ab87866
DongWooKim97 <nanaukim@naver.com>
2023-09-04 10:52:33 +0900

    refactor: override와 print문을 통해 기본함수 및 순서확인, setState 확인

--- statefulWidget/lib/screen/home_screen.dart
+++ statefulWidget/lib/screen/home_screen.dart
@@ -1,29 +1,79 @@
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatefulWidget {
   final Color color;
 
-  const HomeScreen({
+  HomeScreen({
     required this.color,
     Key? key,
-  }) : super(key: key);
+  }) : super(key: key) {
+    print('Widget Constructor 실행!');
+  }
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 50.0,
-      height: 50.0,
-      color: widget.color,
-    );
+  State<HomeScreen> createState() {
+    print('create State 실행 !');
+    return _HomeScreenState();
   }
 }
 
+class _HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  @override
+  void initState() {
+    print('initState 실행!');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies 실행!');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void deactivate() {
+    print('deactivate 실행!');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    // deactivate 및 dispose를 하면, State조차 삭제됨. 따라서 위젯 삭제하기 버튼을 누르면 그동안 Tap하여 올려놨던
+    //number정보들도 다같이 사라짐
+    print('dispose 실행!');
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print('didUpadteWidget 실행!');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build 실행!');
+
+    return GestureDetector(
+      // 제스터디텍터는 child에 감싸져있는 것들만 확인가능!!
+      onTap: () {
+        setState(() {
+          number++;
+        });
+      },
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        color: widget.color,
+        child: Center(
+          child: Text(number.toString()),
+        ),
+      ),
+    );
+  }
+}
 
 // class HomeScreen extends StatelessWidget {
 //   final Color color;
