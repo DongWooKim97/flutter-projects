f90a379730f8247dafea8180fbecb768c2c3c832
DongWooKim97 <nanaukim@naver.com>
2023-09-06 16:12:40 +0900

    feat: 상수 코드 관리 학습

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,44 +12,62 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('랜덤숫자 생성기'),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('123'),
-                  Text('456'),
-                  Text('789'),
+                  Text(
+                    '랜덤숫자 생성기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.settings,
+                      color: RED_COLOR,
+                    ),
+                  ),
                 ],
               ),
-            ),
-            SizedBox(
-              // sizedBox는 Container에 약간의 하위호환
-              width: double.infinity,
-              child: Expanded(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('생성하기!'),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('123'),
+                    Text('456'),
+                    Text('789'),
+                  ],
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                // sizedBox는 Container에 약간의 하위호환
+                width: double.infinity,
+                child: Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          RED_COLOR, // primary = backgroundColor  ,, 일반적인 주된 색깔
+                    ),
+                    onPressed: () {},
+                    child: Text('생성하기!'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
