5e2d800ae2a3468824a21eb049233d632cf554be
DongWooKim97 <nanaukim@naver.com>
2023-09-12 11:24:30 +0900

    feat: 설정페이지에서 result값을 넘겨받아 setState하는 로직 생성

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -12,6 +12,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int maxNumber = 1000;
+
   List<int> randomNumbers = [
     123,
     456,
@@ -23,12 +25,30 @@ class _HomeScreenState extends State<HomeScreen> {
     final Set<int> newNumbers = {};
 
     while (newNumbers.length != 3) {
-      final number = rand.nextInt(1000);
+      final number = rand.nextInt(maxNumber);
 
       newNumbers.add(number);
 
       setState(() {
         randomNumbers = newNumbers.toList();
+      });
+    }
+  }
+
+  void onSettingsPop() async {
+    // list -- add 와 같은 함수 = push // 라우터스택!!
+    // 푸시했던 라우터페이지를 통해 다시 데이터를 돌려받으면 된다.
+    // 다만 , 그냥 받을 순 없고 async로 받아야한다.
+    // 어떤값이 리턴될지 정할 수 있다.제너릭을 통핸
+    final result = await Navigator.of(context).push<int>(
+      MaterialPageRoute(builder: (BuildContext context) {
+        return SettingsScreen();
+      }),
+    ); // 네비게이터 활성화
+
+    if (result != null) {
+      setState(() {
+        maxNumber = result;
       });
     }
   }
@@ -43,7 +63,9 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _Header(),
+              _Header(
+                onPressed: onSettingsPop,
+              ),
               _Body(randomNumbers: randomNumbers),
               _Footer(onPressed: onRandomNumberGenerate),
             ],
@@ -55,7 +77,9 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class _Header extends StatelessWidget {
-  const _Header({super.key});
+  final VoidCallback onPressed;
+
+  const _Header({required this.onPressed, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -71,14 +95,8 @@ class _Header extends StatelessWidget {
           ),
         ),
         IconButton(
-          onPressed: () {
-            // list -- add 와 같은 함수 = push // 라우터스택!!
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (BuildContext context) {
-                return SettingsScreen();
-              }),
-            ); // 네비게이터 활성화
-          },
+          // onPressed를 위로 올려 상태관리를 일괄적으로 해줘야한다.
+          onPressed: onPressed,
           icon: Icon(
             Icons.settings,
             color: RED_COLOR,
@@ -106,8 +124,7 @@ class _Body extends StatelessWidget {
             .asMap()
             .entries
             .map(
-              (x) =>
-              Padding(
+              (x) => Padding(
                 // asMap()을 사용하면 우리는 key값을 활용할 수 있다.
                 padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
                 child: Row(
@@ -115,17 +132,16 @@ class _Body extends StatelessWidget {
                       .toString()
                       .split('')
                       .map(
-                        (y) =>
-                        Image.asset(
+                        (y) => Image.asset(
                           'asset/img/$y.png',
                           height: 70.0,
                           width: 50.0,
                         ),
-                  )
+                      )
                       .toList(),
                 ),
               ),
-        )
+            )
             .toList(),
       ),
     );
@@ -149,7 +165,7 @@ class _Footer extends StatelessWidget {
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
             backgroundColor:
-            RED_COLOR, // primary = backgroundColor  ,, 일반적인 주된 색깔
+                RED_COLOR, // primary = backgroundColor  ,, 일반적인 주된 색깔
           ),
           onPressed: onPressed,
           child: Text('생성하기!'),
--- random_number_generator/lib/screen/settings_screen.dart
+++ random_number_generator/lib/screen/settings_screen.dart
@@ -9,7 +9,7 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  double maxNumber = 10000;
+  double maxNumber = 1000;
 
   @override
   Widget build(BuildContext context) {
@@ -43,8 +43,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 // 슬라이더자체는 동그라미를 움직일때 그 동작을 그려내는 것을 못하므로 우리가 따로 선언 및 어떤 동작을 해줘야한다.
                 // 그렇다면 어떠한 값으로 빌드가 되느냐 ? 우리가 설정한 value : maxValue로 설정해줬기 때문에 maxValue값으로 변경이 된다.
                 value: maxNumber,
-                min: 10000,
-                max: 1000000,
+                min: 1000,
+                max: 100000,
                 onChanged: (double val) {
                   setState(() {
                     maxNumber = val;
@@ -52,7 +52,11 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 },
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  // 현재 화면을 터트린다 -> 뒤로 돌린다. 세팅 페이지에서 설정한 maxNumber를 홈화면에 전달해줘야한다.
+                  // [ T? result] -> Optional 파라미터를 통해 넘기는 값이 result라는 파라미터이다.
+                  Navigator.of(context).pop(maxNumber.toInt());
+                },
                 style: ElevatedButton.styleFrom(
                   primary: RED_COLOR,
                 ),
