e075363969fc270c16cbf8085456ad3eae7bcd72
DongWooKim97 <nanaukim@naver.com>
2023-09-12 18:02:23 +0900

    feat: 알고 있으면 좋은 push 메서드

--- navigation/lib/screen/route_two_screen.dart
+++ navigation/lib/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
@@ -27,8 +28,51 @@ class RouteTwoScreen extends StatelessWidget {
             Navigator.of(context).pushNamed('/three', arguments: 999);
           },
           child: Text('Push Named'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            //[HomeScreen(), RouteOne(), RouteTwo(), RouteThree()] -> 기존의 라우트 스택
+            //[HomeScreen(), RouteOne(), RouteThree()] -> replacement를 사용하면 사용한 스택을 삭제함
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
+              ),
+            );
+          },
+          child: Text(
+            'Push Replacement',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed('/three');
+          },
+          child: Text(
+            'Push ReplacementNamed',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (_) => RouteThreeScreen()),
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: Text('Push And Remove Until'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              '/three',
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: Text('PushNamed And Remove Until'),
         )
       ],
     );
   }
 }
+
+// pushAndRemoveUntil를 실행하는 순간 두번째 파라미터에서 기존에 존재하는 모든 라우터에 각각 false-삭제, true-생존
+// (route) => route.settings.name == '/',   route에 이름이 '/' 이 아닌 것들은 모조리 라우트 스택에서 삭제 -> 홈만 남아서 홈으로 돌아가기
