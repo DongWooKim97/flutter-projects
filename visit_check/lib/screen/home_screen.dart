3b34361e6d57d9f24a85d13b7d52bae798c04a5e
DongWooKim97 <nanaukim@naver.com>
2023-09-14 13:20:08 +0900

    feat: 원 그리기 및 지도에 현재위치 설정 학습

--- visit_check/lib/screen/home_screen.dart
+++ visit_check/lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,13 +10,126 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static final double distance = 100;
+  static final LatLng companyLatLng = LatLng(37.5233273, 126.921252);
+  static final CameraPosition initialPosition = CameraPosition(
+    target: companyLatLng,
+    zoom: 15,
+  );
+
+  //실제로 그릴 원을 생성한다.
+  static final Circle circle = Circle(
+    //id값은 화면을 여러개의 원을 그렸을 때 식별할 수 있는 식별자 역할을 한다.
+    circleId: CircleId('circle'),
+    center: companyLatLng,
+    //투명도를 설정하지 않고 원을 구리면 지도를 불투명하게 가려버린다.
+    fillColor: Colors.blue.withOpacity(0.5),
+    //내부색칠
+    radius: distance,
+    //원의 둘레 색칠
+    strokeColor: Colors.blue,
+    strokeWidth: 1,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Home Screen',
+      appBar: renderAppbar(),
+      body: FutureBuilder(
+        future: checkPermission(), // 무조건 Future를 리턴해주는 함수만을 사용할 수 있다.
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.data == '위치 권한이 허가 되었습니다.') {
+            return Column(
+              children: [
+                _CustomGoogleMap(
+                  initialPosition: initialPosition,
+                  circle: circle,
+                ),
+                _ChoolCheckButton(),
+              ],
+            );
+          }
+          return Center(child: Text(snapshot.data));
+        },
+      ),
+    );
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+
+      if (checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 설정에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가 되었습니다.';
+  }
+
+  AppBar renderAppbar() {
+    return AppBar(
+      title: Text(
+        '오늘도 출근',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
         ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class _CustomGoogleMap extends StatelessWidget {
+  final CameraPosition initialPosition;
+  final Circle circle;
+
+  const _CustomGoogleMap({
+    required this.initialPosition,
+    required this.circle,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 5,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: initialPosition,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
+        //Set이 리턴타입이기에 중복되면 하나로 인식돼서 화면에 하나만 나타난다. 결국은 화면에 동시에 띄우고 싶다면 아까 설정한 circleId를 각각 달리 설정해줘야한다.
+        circles: Set.from([circle]),
+      ),
+    );
+  }
+}
+
+class _ChoolCheckButton extends StatelessWidget {
+  const _ChoolCheckButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text(
+        '출근',
       ),
     );
   }
