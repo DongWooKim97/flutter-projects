9886249b4603fba6e6161cd4dc1e00b1efc70cc7
DongWooKim97 <nanaukim@naver.com>
2023-09-14 17:16:53 +0900

    feat: Stream Builder에 대한 학습

--- future_and_stream_builder/lib/screen/home_screen.dart
+++ future_and_stream_builder/lib/screen/home_screen.dart
@@ -19,18 +19,13 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: FutureBuilder(
+        child: StreamBuilder<int>(
+          // 제네릭을 사용해도 되고 안해도 되는데, 하게된다면 snapshot.data에 들어가는 데이터타입을 명시해줘야함
           // 퓨처빌더는 기존의 데이터값을 기억함. 이것이 "캐싱". 그래서 done되기 전까지는 snapshot.data의 값이 바뀌지 않음.
-          future: getNumber(),
-          builder: (context, snapshot) {
+          stream: streamNumbers(),
+          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+            // 스냅샷 타입 직접 명시
             //기본적으로 많이하는 방식
-            if (snapshot.hasData) {
-              //데이터가 있을 때 위젯 렌더링
-            }
-
-            if (snapshot.hasError) {
-              // 에러가 났을 때 위젯 렌더링
-            }
             // 둘 다 아닐 때 = 로딩중일때 위젯 렌더링
             // 스냅샷 값이 바뀔 때도 재실행 +setState가 실행되도 재실행
             return Column(
@@ -38,7 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Text(
-                  'Future Builder',
+                  'Stream Builder',
                   style: textStyle.copyWith(
                     fontWeight: FontWeight.w700,
                     fontSize: 20.0,
@@ -78,6 +73,16 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return random.nextInt(100);
   }
+
+  Stream<int> streamNumbers() async* {
+    for (int i = 0; i < 10; i++) {
+      if (i == 5) {
+        throw Exception('i == 5');
+      }
+      await Future.delayed(Duration(seconds: 1));
+      yield i;
+    }
+  }
 }
 
 // Future build에는 당연하게도 future함수가 들어가야한다 .
@@ -85,3 +90,6 @@ class _HomeScreenState extends State<HomeScreen> {
 // 빌더함수를 다시 실행하는 순간은 setState가 실행되어 builder함수가 다시 한번 실행되는 경우가 있다.
 
 // 중요한건 로딩중일 때 캐싱을 통해 로딩중이 아니라고 유저로 하여금 착각을 불러올 수 있어 앱이 느리다고 판단할 수 없게끔 눈속임이 중요하다
+
+// stream에서 함수가 끝나지 않았을 때 = 아직 값을 받고 있을때가 == ConnectionState.active
+// StreamBuilder는  Future에서 처럼 dispose를 직접 명시해줄 필요가 없다. 왜냐 모든 로직이 끝나고 dispose되는것 까지가 하나의 로직이기 때문에
