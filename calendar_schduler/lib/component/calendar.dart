e94b8c8e2ddfb89e51d04900351a2df8010a48ae
DongWooKim97 <nanaukim@naver.com>
2023-09-15 14:34:45 +0900

    style: 1차 캘린더 스타일 꾸미기

--- calendar_schduler/lib/component/calendar.dart
+++ calendar_schduler/lib/component/calendar.dart
@@ -1,15 +1,34 @@
+import 'package:calendar_schduler/constant/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime? selectedDay;
+  final defaultBoxDeco = BoxDecoration(
+    borderRadius: BorderRadius.circular(60.0),
+    color: Colors.grey[200],
+  );
+  final defaultTextStyle = TextStyle(
+    color: Colors.grey[600],
+    fontWeight: FontWeight.w700,
+  );
 
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
-      focusedDay: DateTime.now(), // 단순이 몇월을 보여줄지 결정하는 값
-      firstDay: DateTime(1800), //가장 첫번째 날짜를 언제할건지 결정
-      lastDay: DateTime(3000), //제일 미래의 날짜를 최대한 언제까지 결정
+      focusedDay: DateTime.now(),
+      // 단순이 몇월을 보여줄지 결정하는 값
+      firstDay: DateTime(1800),
+      //가장 첫번째 날짜를 언제할건지 결정
+      lastDay: DateTime(3000),
+      //제일 미래의 날짜를 최대한 언제까지 결정
       headerStyle: HeaderStyle(
         formatButtonVisible: false,
         titleCentered: true,
@@ -18,8 +37,42 @@ class Calendar extends StatelessWidget {
           fontSize: 16.0,
         ),
       ),
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: true,
+        defaultDecoration: defaultBoxDeco,
+        // 평일박스데코
+        weekendDecoration: defaultBoxDeco,
+        // 주말박스데코
+        selectedDecoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(60.0),
+          border: Border.all(
+            width: 1.0,
+            color: PRIMARY_COLOR,
+          ),
+        ),
+        defaultTextStyle: defaultTextStyle,
+        weekendTextStyle: defaultTextStyle,
+        selectedTextStyle: defaultTextStyle.copyWith(
+          color: PRIMARY_COLOR,
+        ),
+      ),
       onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+        // 어떤 날짜가 선택되었는지를 가지고 있어야하니까 stateful하게해야함
+        // 슬라이더처럼 선택된 날짜가 얼마인지 다시 값을 넘겨서 build해줘야한다!!!!
+        setState(() {
+          this.selectedDay = selectedDay;
+        });
+      },
+      selectedDayPredicate: (DateTime date) {
+        //date == selectedDay를 안하는 이유는 이렇게 하면 시/분/초 까지 다 비교하기때문에. 우리는 그럴 필요는 없었기에 사용하지 않음.
+        if (selectedDay == null) {
+          return false;
+        }
 
+        return date.year == selectedDay!.year &&
+            date.month == selectedDay!.month &&
+            date.day == selectedDay!.day;
       },
     );
   }
