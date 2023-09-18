57008211aeded5027d64d45ed5b86dc0739114fd
DongWooKim97 <nanaukim@naver.com>
2023-09-18 10:51:50 +0900

    feat: wrap위젯 사용하기

---
+++ calendar_schduler/lib/component/custom_text_field.dart
@@ -0,0 +1,32 @@
+import 'package:calendar_schduler/constant/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+
+  const CustomTextField({required this.label, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextField(
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            filled: true,
+            fillColor: Colors.grey[300],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- calendar_schduler/lib/component/schedule_bottom_sheet.dart
+++ calendar_schduler/lib/component/schedule_bottom_sheet.dart
@@ -1,3 +1,5 @@
+import 'package:calendar_schduler/component/custom_text_field.dart';
+import 'package:calendar_schduler/constant/colors.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleBottomSheet extends StatelessWidget {
@@ -13,13 +15,115 @@ class ScheduleBottomSheet extends StatelessWidget {
       height: MediaQuery.of(context).size.height / 2 + bottomInset,
       color: Colors.white,
       child: Padding(
-        padding:  EdgeInsets.only(bottom: bottomInset),
-        child: Column(
-          children: [
-            TextField(),
-          ],
+        padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Time(),
+              SizedBox(height: 16.0),
+              _Content(),
+              SizedBox(height: 16.0),
+              _ColorPicker(),
+              SizedBox(height: 8.0),
+              _SaveButton(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _ColorPicker extends StatelessWidget {
+  const _ColorPicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      // 파라미터 - spacing
+      spacing: 8.0, // 좌우 관격
+      runSpacing: 10.0, // 위아래 간격
+      children: [
+        renderColor(Colors.red),
+        renderColor(Colors.orange),
+        renderColor(Colors.yellow),
+        renderColor(Colors.green),
+        renderColor(Colors.blue),
+        renderColor(Colors.indigo),
+        renderColor(Colors.purple),
+      ],
+    );
+  }
+
+  Widget renderColor(Color color) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+      width: 32.0,
+      height: 32.0,
+    );
+  }
+}
+
+class _Time extends StatelessWidget {
+  const _Time({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // 그냥 ROW에다 children들을 CustomTextField로 넣으면 얼마나 크기를 차지해야하는지 모르기때문에 오류발생 -> expanded필요
+        Expanded(
+          child: CustomTextField(
+            label: '시작 시간',
+          ),
+        ),
+        SizedBox(width: 16.0),
+        Expanded(
+          child: CustomTextField(
+            label: '마감 시간',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: CustomTextField(
+        label: '내용',
+      ),
+    );
+  }
+}
+
+class _SaveButton extends StatelessWidget {
+  const _SaveButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            // 버튼을 꽉차게 만드는 방법 여러가지 -> Row로 감싼 후 Expanded!
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: PRIMARY_COLOR,
+            ),
+            child: Text('저장'),
+          ),
+        ),
+      ],
     );
   }
 }
