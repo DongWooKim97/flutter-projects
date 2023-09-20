e5d540f3bc62294a4935f428bfbe0bec597f1195
DongWooKim97 <nanaukim@naver.com>
2023-09-20 16:25:47 +0900

    refactor: home_Screen 대공사 1차전

--- ui_practice/lib/screen/home_screen.dart
+++ ui_practice/lib/screen/home_screen.dart
@@ -33,40 +33,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 300,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black,
-              width: 2.0,
-            ),
-            borderRadius: BorderRadius.circular(2.0),
+      body: Container(
+        width: MediaQuery.of(context).size.width / 4,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 2.0,
           ),
-          child: Column(
-            children: [
-              ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: depthColors.length,
-                itemBuilder: (context, index) {
-                  final depthKey = depthColors.keys.elementAt(index);
-                  final depthValue = depthColors[depthKey];
-                  return renderDepthItem(depthKey, depthValue!);
-                },
-              ),
-              const SizedBox(height: 300),
-              Visibility(
-                visible: _colorPickerVisible,
-                child: ColorPicker(
-                  pickerColor: Colors.black,
-                  onColorChanged: (color) {
-                    print('헬로');
+          borderRadius: BorderRadius.circular(2.0),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              child: SingleChildScrollView(
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: depthColors.length,
+                  itemBuilder: (context, index) {
+                    final depthKey = depthColors.keys.elementAt(index);
+                    final depthValue = depthColors[depthKey];
+                    return renderDepthItem(depthKey, depthValue!);
                   },
                 ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 100),
+            const Divider(
+              color: Colors.black,
+              thickness: 1.0,
+            ),
+          ],
         ),
       ),
     );
@@ -74,14 +72,21 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget renderDepthItem(String depthKey, Color depthValue) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.only(
+          left: 32.0, right: 32.0, bottom: 16.0, top: 16.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(depthKey),
+          Text(
+            depthKey,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
           const SizedBox(width: 20.0),
           ElevatedButton(
-            onPressed: () => {!_colorPickerVisible ? _show() : _hide()},
+            onPressed: _DepthColorPicker(colorPickerVisible: _colorPickerVisible),
             style: ElevatedButton.styleFrom(
               backgroundColor: depthValue,
             ),
@@ -92,3 +97,47 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+class _DepthColorPicker extends StatefulWidget {
+  final bool colorPickerVisible;
+
+  const _DepthColorPicker({required this.colorPickerVisible, super.key});
+
+  @override
+  State<_DepthColorPicker> createState() => _DepthColorPickerState();
+}
+
+class _DepthColorPickerState extends State<_DepthColorPicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: true,
+      child: SingleChildScrollView(
+        child: ColorPicker(
+          colorPickerWidth: MediaQuery.of(context).size.width / 12.5,
+          labelTypes: const [ColorLabelType.rgb],
+          paletteType: PaletteType.hsvWithHue,
+          pickerAreaHeightPercent: 1.2,
+          pickerColor: Colors.black,
+          enableAlpha: true,
+          onColorChanged: (color) {
+            print('헬로');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+/*
+
+ElevatedButton(
+            onPressed: () => {!_colorPickerVisible ? _show() : _hide()},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: depthValue,
+            ),
+            child: null,
+          ),
+ */
