f2610228773dab321cc689798087e1ff21e1bcf3
DongWooKim97 <nanaukim@naver.com>
2023-09-25 16:28:06 +0900

    refactor: ListView 공부

---
+++ scrollable_widgets/lib/screen/list_view_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/constant/colors.dart';
+import 'package:scrollable_widgets/layout/main_layout.dart';
+
+class ListViewScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  ListViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'ListViewScreen',
+      body: renderSeparate(),
+    );
+  }
+
+  Widget renderSeparate() {
+    return ListView.separated(
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      },
+      separatorBuilder: (context, index) {
+        ++index; // 인덱스 맞추기 구색용
+        return (index % 5 == 0)
+            ? renderContainer(color: Colors.black, index: index, height: 100)
+            : Container(); // 컨테이너를 선언하고 크기를 조절하지 않으면 그냥 아무것도 없음
+      },
+    );
+  }
+
+  Widget renderBuilder() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      },
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+    return Container(
+      height: height ?? 300,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+//ListView.separated 는 결국 List사이에 구분선같은 역할을 하는 것들을 넣어줄 수 있는
+// separateBuilder가 있다.  이 builder또한 기존의 builder를 작성하는 것처럼 작성할 수 있다.
+// banner나 광고를 넣을 떄 엄청 유용하다.
