000bb4a7160ef1d0ac07c2f292ab64ff13eab7be
김동우 <nanaukim@naver.com>
2023-10-01 21:31:53 +0900

    feat: ScrollBar & RefreshIndicator 활용

---
+++ scrollable_widgets/lib/screen/scrollbar_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/constant/colors.dart';
+import 'package:scrollable_widgets/layout/main_layout.dart';
+
+class ScrollbarScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  ScrollbarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'ScrollbarScreen',
+      body: Scrollbar( // 투명한 기본 스크롤바 추가
+        child: SingleChildScrollView(
+          child: Column(
+            children: numbers
+                .map(
+                  (e) => renderContainer(
+                    color: rainbowColors[e % rainbowColors.length],
+                    index: e,
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+    return Container(
+      height: height ?? 300,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
